558fe70340c23b447d1375b6e27e4ae699c72607
Christos Anastopoulos <christos.anastopoulos@cern.ch>
2020-08-04 00:39:18 +0100

    update bench

--- run.sh
+++ run.sh
@@ -1,7 +1,2 @@
-cd benchmark/
-mkdir build && cd build
-cmake -DCMAKE_BUILD_TYPE=Release  -DBENCHMARK_ENABLE_GTEST_TESTS=OFF ../
-make
-cd ../../
 g++ findMinumumIndex_bench.cxx -std=c++17 -march=native  -O3 -isystem benchmark/include -Lbenchmark/build/src -lbenchmark -lpthread -o mybenchmark
 ./mybenchmark --benchmark_report_aggregates_only=true --benchmark_repetitions=20
