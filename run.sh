2391b3a54e16d64dbd4a1bdb798e96052212623b
Christos Anastopoulos <christos.anastopoulos@cern.ch>
2020-08-26 23:09:16 +0100

    remove old scripts

--- run.sh
+++
@@ -1,2 +0,0 @@
-g++ findMinumumIndex_bench.cxx -std=c++17 -march=native  -O3 -isystem benchmark/include -Lbenchmark/build/src -lbenchmark -lpthread -o mybenchmark
-./mybenchmark --benchmark_report_aggregates_only=true --benchmark_repetitions=20
