0de705ccb88b976286f152e171bbd95940b2ce97
Christos Anastopoulos <christos.anastopoulos@cern.ch>
2020-08-26 22:26:18 +0100

    Remove KL for now

--- KL/test.sh
+++
@@ -1,2 +0,0 @@
-g++ -O3   -march=native  -Wall -ftree-vectorize -std=c++17 KLGaussianMixtureReduction.cxx -c -o KLGaussianMixtureReductions.o
-g++ testMergeComponents.cxx KLGaussianMixtureReductions.o -O3 -Wall  -std=c++17 -isystem ../benchmark/include -L../benchmark/build/src -lbenchmark -lpthread -o test
