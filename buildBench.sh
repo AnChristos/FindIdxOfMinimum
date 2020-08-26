71fa76bd141ed4fd123f50f8cab0b7a4335cc772
Christos Anastopoulos <christos.anastopoulos@cern.ch>
2020-08-26 22:29:11 +0100

    Update  scripts

--- buildBench.sh
+++ buildBench.sh
@@ -1,5 +1,2 @@
-cd benchmark/
-mkdir build && cd build
-cmake -DCMAKE_BUILD_TYPE=Release  -DBENCHMARK_ENABLE_GTEST_TESTS=OFF ../
-make
-cd ../../
+g++ findMinumumIndex_bench.cxx -std=c++17  -O2 -isystem benchmark/include -Lbenchmark/build/src -lbenchmark -lpthread -o findMinumumIndex_bench
+#./findMinumumIndex_bench --benchmark_report_aggregates_only=true --benchmark_repetitions=20
---
+++ buildTest.sh
@@ -0,0 +1,1 @@
+g++ findMinumumIndex_test.cxx  -std=c++17   -O2 -o findMinumumIndex_test
