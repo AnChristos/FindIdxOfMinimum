56d78f4b1358631b85ea598f8cd31ba142cd82df
Christos Anastopoulos <christos.anastopoulos@cern.ch>
2020-09-11 01:17:35 +0100

    remove obsolete

--- buildBench.sh
+++
@@ -1,2 +0,0 @@
-g++ findMinumumIndex_bench.cxx -std=c++17  -O2 -isystem benchmark/include -Lbenchmark/build/src -lbenchmark -lpthread -o findMinumumIndex_bench
-#./findMinumumIndex_bench --benchmark_report_aggregates_only=true --benchmark_repetitions=20
--- buildTest.sh
+++
@@ -1,1 +0,0 @@
-g++ findMinumumIndex_test.cxx  -std=c++17   -O2 -o findMinumumIndex_test
